3e436730c98ed71dff2142a2145d812d0bc14e04
Mikkel Bistrup Andersen <mbia@itu.dk>
2023-11-30 11:56:34 +0100

    fixed bugs and spelling errors

--- MicroC/Absyn.fs
+++ MicroC/Absyn.fs
@@ -23,7 +23,7 @@ and expr =
   | Andalso of expr * expr           (* Sequential and              *)
   | Orelse of expr * expr            (* Sequential or               *)
   | Call of string * expr list       (* Function call f(...)        *)
-  | Cont of expr * expr * expr       (* trinary*)
+  | Cond of expr * expr * expr       (* trinary*)
                                                                    
 and access =                                                       
   | AccVar of string                 (* Variable access        x    *) 
